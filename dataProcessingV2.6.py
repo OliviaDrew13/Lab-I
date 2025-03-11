84174f12d32888ac1559ce619c9da85a9b5b5634
BaguetteFeet <braday2005@gmail.com>
2025-03-11 14:04:41 +0000

    played around to try to fix

--- dataProcessingV2.6.py
+++ dataProcessingV2.6.py
@@ -15,8 +15,10 @@ from scipy import signal
 plt.rcParams['axes.autolimit_mode'] = 'round_numbers'
 plt.rcParams["axes.formatter.limits"] = -2, 2
 
-def plotting(x, y, xyLabels, labels, types, labelsOn = True, xExtents = None, yExtents = None, grid = True, title = None, sharex = False):
+def plotting(x, y, xyLabels, labels, types, labelsOn = True, xExtents = None, yExtents = None, grid = True, title = None, sharex = False, aspect = None):
     
+    
+    title = None
     fig, ax = plt.subplots()
         
     if np.shape(labels) == ():
@@ -62,6 +64,9 @@ def plotting(x, y, xyLabels, labels, types, labelsOn = True, xExtents = None, yE
     ax.set_ylabel(xyLabels[1])
     ax.set_title(title)
     
+    if aspect != None:
+        ax.set_aspect("equal")
+    
     # Setting the plotting extents if they are specified
     if xExtents != None:
         ax.set_xlim(xExtents[0], xExtents[1])
@@ -71,6 +76,8 @@ def plotting(x, y, xyLabels, labels, types, labelsOn = True, xExtents = None, yE
     # Applying legend if true
     if labelsOn == True:
         ax.legend()
+    
+    plt.show()
     
 def subplotting(x, y, xylabels, labels=None, xExtents=None, yExtents=None, labelsOn=True, grid = True, figsize = (6,5), title=None):
     
@@ -90,19 +97,22 @@ def subplotting(x, y, xylabels, labels=None, xExtents=None, yExtents=None, label
             axs[i].grid(which='minor', color='k', linestyle='--', linewidth=0.4, alpha = 0.4)
             axs[i].minorticks_on()
         
-        # Setting the plotting extents if they are specified
-        if xExtents != None:
-            axs[i].set_xlim(xExtents[i][0], xExtents[i][1])
-        if yExtents != None:
-            axs[i].set_ylim(yExtents[i][0], yExtents[i][1])
+# =============================================================================
+#         # Setting the plotting extents if they are specified
+#         if xExtents != None:
+#             axs[i].set_xlim(xExtents[i][0], xExtents[i][1])
+#         if yExtents != None:
+#             axs[i].set_ylim(yExtents[i][0], yExtents[i][1])
+# =============================================================================
         
         # Applying legend if true
         if labelsOn == True:
             axs[i].legend(lines, labels[i])
             print(labels[i])
-        
-        axs[0].set_title(title)
-        fig.tight_layout()    
+    
+    axs[0].set_title(" \n ")
+    
+    fig.tight_layout()    
 
 def rotation(time, aX, aY, aZ, gX, gY, gZ):
     
@@ -110,13 +120,13 @@ def rotation(time, aX, aY, aZ, gX, gY, gZ):
     gX, gY, gZ = -gX, -gY, -gZ
     
     #finding the respective rotational angles in radians
-    rX = np.deg2rad(scI.cumulative_trapezoid(gX, None, dt))
-    rY = np.deg2rad(scI.cumulative_trapezoid(gY, None, dt))
-    rZ = np.deg2rad(scI.cumulative_trapezoid(gZ, None, dt))
+    rX = np.deg2rad(scI.cumulative_trapezoid(gX, None, dt, initial=0))
+    rY = np.deg2rad(scI.cumulative_trapezoid(gY, None, dt, initial=0))
+    rZ = np.deg2rad(scI.cumulative_trapezoid(gZ, None, dt, initial=0))
     # rX = np.deg2rad(gX)
     # rY = np.deg2rad(gY)
     # rZ = np.deg2rad(gZ)
-
+    print(len(rX))
     #setting up acceleration vectors
     rAX = []
     rAY = []
@@ -124,7 +134,7 @@ def rotation(time, aX, aY, aZ, gX, gY, gZ):
     
     gravity = 9.81
     
-    for i in range(len(aX)-1):
+    for i in range(len(aX)):
         accVec = []
         accVec = np.array([aX[i], aY[i], aZ[i]])
         rotMatX = np.array([[1, 0, 0], [0, np.cos(rX[i]), -np.sin(rX[i])], [0, np.sin(rX[i]), np.cos(rX[i])]])
@@ -150,8 +160,8 @@ def rotation(time, aX, aY, aZ, gX, gY, gZ):
     # rAZ = np.array(rAZ)-gravity
     
     # plotting([time.iloc[1:],time.iloc[1:],time.iloc[1:]], [rAX,rAY,rAZ], ["Time (s)", "Acceleration (m/s$^2$)"], ['$a_y$','$a_y$','$a_z$'], ["pl", "pl", "pl"])
-    
-    return(time[1:],rAX,rAY,rAZ)
+    print(len(rAX))
+    return(time,rAX,rAY,rAZ)
 
 def integrationTrapezoid(time,X,Y,Z,dt):
     # Integrating the input data
@@ -159,13 +169,11 @@ def integrationTrapezoid(time,X,Y,Z,dt):
     # YI = scI.cumulative_trapezoid(Y,None,dt)
     # ZI = scI.cumulative_trapezoid(Z,None,dt)
     
-    XI = scI.cumulative_trapezoid(X,time)
-    YI = scI.cumulative_trapezoid(Y,time)
-    ZI = scI.cumulative_trapezoid(Z,time)
+    XI = scI.cumulative_trapezoid(X,time, initial=0)
+    YI = scI.cumulative_trapezoid(Y,time, initial=0)
+    ZI = scI.cumulative_trapezoid(Z,time, initial=0)
     
-    timeI = time[1:]
-    
-    return(timeI, XI, YI, ZI)
+    return(time, XI, YI, ZI)
 
 def plotting3D(x, y, z, axisLabels):
     
@@ -182,7 +190,6 @@ def plotting3D(x, y, z, axisLabels):
     
 def euler(time, aX, aY, aZ, dt):
     
-    
     vx=[0]
     vy=[0]
     vz=[0]
@@ -192,9 +199,7 @@ def euler(time, aX, aY, aZ, dt):
         vy.append(vy[i]+(aY[i] * dt))
         vz.append(vz[i]+(aZ[i] * dt))
         
-    timeI = time
-        
-    return(timeI, np.array(vx[1:]), np.array(vy[1:]), np.array(vz[1:]))
+    return(time, np.array(vx[1:]), np.array(vy[1:]), np.array(vz[1:]))
 
 def savgolFiltering(ax,ay,az):
     
@@ -226,7 +231,7 @@ def butterLow(time, x, y, z, lowF):
 
 def dataCalibration(time, ax, ay, az, gx, gy, gz):
     
-    calibIndices = np.where(time<=2)
+    calibIndices = (time<=2)
     
     variables = [ax[calibIndices], ay[calibIndices], az[calibIndices], gx[calibIndices], gy[calibIndices], gz[calibIndices]]
     corrections = []
@@ -239,8 +244,7 @@ def dataCalibration(time, ax, ay, az, gx, gy, gz):
         
     return np.array(corrections)
     
-    
-filePaths = ["DataDay1/xMoveTest1_25_02_2025_Data.txt"]
+filePaths = ["DataDay1/rotX1_25_02_2025_Data.txt"]
 # filePaths = ["DataDay2SRFix/positiveX1_04_03_2025_Data.txt", "DataDay2SRFix/negativeX1_04_03_2025_Data.txt", "DataDay2SRFix/positiveY1_04_03_2025_Data.txt", "DataDay2SRFix/negativeY1_04_03_2025_Data.txt", "DataDay2SRFix/positiveZ1_04_03_2025_Data.txt", "DataDay2SRFix/negativeZ1_04_03_2025_Data.txt"]
 
 # Day2
@@ -256,32 +260,29 @@ for file in filePaths:
     
     # Reading raw data
     t, ax, ay, az, gx, gy, gz = np.array(dataF["Time"]), dataF["ax"], dataF["ay"], dataF["az"], dataF["gx"], dataF["gy"], dataF["gz"]
+    dataArrays = [ax, ay, az, gx, gy, gz]
     
     dt = 0.001
     
     print(f"sample rate = {len(ax)/t.max()}")
     
-    # Filtering the data with Savitzky-Golay
-    fAX, fAY, fAZ = savgolFiltering(ax,ay,az)
-    fGX, fGY, fGZ = savgolFiltering(gx,gy,gz)
-    dataArrays = [fAX, fAY, fAZ, fGX, fGY, fGZ]
+    corrections = dataCalibration(t, ax, ay, az, gx, gy, gz)
     
-# =============================================================================
-#     # Filtering the data with Butterworth lowpass
-#     freq = 20
-#     fAX, fAY, fAZ = butterLow(t, ax, ay, az, freq)
-#     fGX, fGY, fGZ = butterLow(t, gx, gy, gz, freq)
-#     dataArrays = [fAX, fAY, fAZ, fGX, fGY, fGZ]
-# =============================================================================
-    
-    corrections = dataCalibration(t, fAX, fAY, fAZ, fGX, fGY, fGZ)
-
     # Applying measured correciton factors
     for i in range(len(dataArrays)):
         dataArrays[i], lowerMeans[i], upperMeans[i] = correcting(dataArrays[i], corrections[i][0], corrections[i][1])
     
     # Reassigning the acceleration and angular velocity arrays
-    fAX, fAY, fAZ, fGX, fGY, fGZ = dataArrays
+    cAX, cAY, cAZ, cGX, cGY, cGZ = dataArrays
+    
+    # Filtering the data with Savitzky-Golay
+    fAX, fAY, fAZ = savgolFiltering(cAX, cAY, cAZ)
+    fGX, fGY, fGZ = savgolFiltering(cGX, cGY, cGZ)
+    
+    # Filtering the data with Butterworth lowpass
+    freq = 20
+    fAX, fAY, fAZ = butterLow(t, cAX, cAY, cAZ, freq)
+    fGX, fGY, fGZ = butterLow(t, cGX, cGY, cGZ, freq)
     
     # Integrating the filtered acceleration data
     vTime, vx, vy, vz = integrationTrapezoid(t, fAX, fAY, fAZ, dt)
@@ -299,7 +300,7 @@ for file in filePaths:
 #     rvTime, rVX, rVY, rVZ = euler(raTime, rAX, rAY, rAZ, dt)
 #     rposTime, rX,rY,rZ = euler(rvTime, rVX, rVY, rVZ, dt)
 # =============================================================================
-    
+     
     # Index      = [0     , 1  , 2  , 3  , 4     , 5  , 6  , 7  , 8       , 9 , 10, 11]
     rotatedData  = [raTime, rAX, rAY, rAZ, rvTime, rVX, rVY, rVZ, rposTime, rX, rY, rZ]
     filteredData = [t     , fAX, fAY, fAZ, vTime , vx , vy , vz , posTime , x , y , z ]
@@ -307,25 +308,30 @@ for file in filePaths:
     # Plotting the data
     
     # 2D rotated vs unrotated positions
-    plotting([x, rX], [y, rY], ["x (m)", "y(m)"], ["Raw", "Rotated"], "p")
-    
+    plotting([x, rX], [y, rY], ["x (m)", "y(m)"], ["Raw", "Rotated"], "p", aspect = 1)
+    # print(rY.max())
     # Rotated positions vs time
     plotting(rposTime, rotatedData[9:12], ["Time (s)", "Position (m)"], ["$x_R$", "$y_R$", "$z_R$"], "p", sharex = True, title = "Rotated Positions")
+    plotting(t, [gx, gy, gz], ["Time (s)", "Angular Velocity (m)"], ["$x$", "$y$", "$z$"], "p", sharex = True, title = "Rotated Positions")
     
     # Rotated Accelerations
     plotting(raTime, rotatedData[1:4], ["Time (s)", "Acceleration (m/s$^2$)"], ["$A_Rx$", "$A_Ry$", "$A_Rz$"], "p", title= "Rotated Accelerations", sharex = True)
     
     # Filter Demonstrations
-    plotting(t, [ax, fAX], ["time", "acceleration"], ["unfiltered", "filtered"], "p", title = "Filter Demonstration x", sharex = True)
-    plotting(t, [ay, fAY], ["time", "acceleration"], ["unfiltered", "filtered"], "p", title = "Filter Demonstration y", sharex = True)
-    plotting(t, [az, fAZ], ["time", "acceleration"], ["unfiltered", "filtered"], "p", title = "Filter Demonstration z", sharex = True)
+    plotting(t, [fAX, rAX], ["Time (s)", "Acceleration (m/s$^2$)"], ["Filtered", "Rotated"], "p", title = "Filter Demonstration x", sharex = True)
+    plotting(t, [ay, fAY], ["Time (s)", "Acceleration (m/s$^2$)"], ["unfiltered", "filtered"], "p", title = "Filter Demonstration y", sharex = True)
+    plotting(t, [az, fAZ], ["Time (s)", "Acceleration (m/s$^2$)"], ["unfiltered", "filtered"], "p", title = "Filter Demonstration z", sharex = True)
+    
     
     # Plotting the consecutive integrations
-    subplotting([[raTime], [rvTime], [rposTime]], [[rAX], [rVX], [rX]], ["Time (s)", ["Acceleration (m/s$^2$)", "Velocity (m/s)", "Displacement (m)"]], labelsOn = False, title = "Integration in $x$")
+    subplotting([[raTime], [rvTime], [rposTime]], [[rAX], [rVX], [rX]], ["Time (s)", ["Acceleration (m/s$^2$)", "Velocity (m/s)", "Displacement (m)"]], labelsOn = False, title = "")
     subplotting([[raTime], [rvTime], [rposTime]], [[rAY], [rVY], [rY]], ["Time (s)", ["Acceleration (m/s$^2$)", "Velocity (m/s)", "Displacement (m)"]], labelsOn = False, title = "Integration in $y$")
     subplotting([[raTime], [rvTime], [rposTime]], [[rAZ], [rVZ], [rZ]], ["Time (s)", ["Acceleration (m/s$^2$)", "Velocity (m/s)", "Displacement (m)"]], labelsOn = False, title = "Integration in $z$")
     
     # Plotting the path in 3d
     # plotting3D(rX, rY, rZ, ["$x_R$", "$y_R$", "$z_R$"])
+    
+    plotting(t, [ax, cAX], ["Time (s)", "Acceleration (m/s$^2$)"], ["Raw Data", "Corrected Data"], "p", sharex = True)
+    subplotting([[t], [t]], [[fAY], [rAY]], ["Time (s)", ["Acceleration (m/s$^2$)", "Acceleration (m/s$^2$)"]], [["Filtered"], ["Rotated"]])
     
     print(f"The max acceleration is: {fAZ.max()}\nThe max angular velocity is: {fGZ.max()}")
